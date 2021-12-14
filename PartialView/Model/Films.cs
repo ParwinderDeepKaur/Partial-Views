c2dfbfed90d5513c43d811b6509cb08f52a611b5
ParwinderDeepKaur <A00237487@mycambrian.ca>
2021-12-13 22:16:07 -0500

    Assignment Partial View

---
+++ PartialView/Model/Films.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialView.Model
+{
+    public class Films
+    {
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Duration
+        /// </summary>
+        public string Duration { get; set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Description { get; set; }
+
+        
+    }
+}
---
+++ PartialView/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using PartialView.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialView.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        List<Films> Films = new List<Films>
+        {
+            new Films { Title = "The Avengers", Duration = "3 hour", Description = "Nick Fury is compelled to launch the Avengers Initiative when Loki poses a threat to planet Earth. His squad of superheroes put their minds together to accomplish the task." },
+            new Films { Title = "Avengers endgame", Duration = "3 hour", Description = "After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance." },
+            new Films { Title = "Avengers: infinity war", Duration = "3 hour", Description = "The Avengers must stop Thanos, an intergalactic warlord, from getting his hands on all the infinity stones. However, Thanos is prepared to go to any lengths to carry out his insane plan." }
+          };
+
+        public List<Films> FilmList { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            FilmList = Films;
+        }
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
